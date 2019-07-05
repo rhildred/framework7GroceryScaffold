042b4a9f92a5c304b1b63a52cddf920461a4ece4
rhildred <rhildred@gmail.com>
2019-07-05 08:31:44 -0400

    shouldn't have committed this

--- public/firebase.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyCh1dPXUlJ1V2XARNENb3KTIwlGhoKZV3A",
-  authDomain: "todoapp-c25eb.firebaseapp.com",
-  databaseURL: "https://todoapp-c25eb.firebaseio.com",
-  projectId: "todoapp-c25eb",
-  storageBucket: "todoapp-c25eb.appspot.com",
-  messagingSenderId: "155233234380",
-  appId: "1:155233234380:web:5cedc646c6035e1d"
-};
